066288958bad46704ba263794b5ae3a46c516854
itmarkaz2024 <itmarkaz9876@gmail.com>
2024-12-21 14:31:20 +0500

    style

--- src/components/fullNew/FullNew.jsx
+++ src/components/fullNew/FullNew.jsx
@@ -5,20 +5,29 @@ import NewsSidebar from "./NewsSidebar";
 import { IoCalendar } from "react-icons/io5";
 
 function FullNew() {
+  const [loader, setLoader] = useState(false);
   const { id } = useParams();
   const [news, setNews] = useState(false);
   useEffect(() => {
+    setLoader(true);
     fetch(`https://admin-panel-production-abfb.up.railway.app/new/${id}`)
       .then((res) => res.json())
       .then((data) => {
         setNews(data);
+        setLoader(false);
         window.scrollTo(0, 0);
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setLoader(false);
+      });
   }, [id]);
 
   return (
     <div className="full-new-container">
+      <div className={loader ? "loader-container" : "none"}>
+        <span class="loader"></span>
+      </div>
       {news && (
         <div className="full-new">
           <h3 className="fullnew-date">
